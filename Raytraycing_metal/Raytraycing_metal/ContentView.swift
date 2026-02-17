b2ae292b28432c4e96f676c26ec4290c03a173f7
jakduCommand <dyfn248@gmail.com>
2026-02-16 20:12:11 -0800

    add gitignore

---
+++ Raytraycing_metal/Raytraycing_metal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Raytraycing_metal
+//
+//  Created by Jungwoon Ko on 2/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Raytraycing_metal/Raytraycing_metal/Raytraycing_metalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Raytraycing_metalApp.swift
+//  Raytraycing_metal
+//
+//  Created by Jungwoon Ko on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Raytraycing_metalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
